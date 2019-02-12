5b9a88c19d14fd2aabac4e7fc7e1d5c5c75de236
Elizabeth Kimita <kimita.wanjiku@gmail.com>
2019-02-12 15:05:34 +0300

    Adding initial caculator setups

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+var add =function(number1, number2) {
+return number1 + number2;
+};
+alert (add(10, 15));
+var subtract = function(number1, number2) {return number1 - number2;};
+alert (subtract (100, 80));
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+alert(add(number1, number2));
