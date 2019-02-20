6cefcf0336efc761bfe8abb57408fc0339afa78a
lizKimita <kimita.wanjiku@gmail.com>
2019-02-20 12:15:58 +0300

    adding calculator changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,23 @@
-var add =function(number1, number2) {
-return number1 + number2;
+// Business (or Back-end) Logic:
+var add = function(number1, number2) {
+  return number1 + number2;
 };
-alert (add(10, 15));
-var subtract = function(number1, number2) {return number1 - number2;};
-alert (subtract (100, 80));
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
-alert(add(number1, number2));
+
+var subtract = function(number1, number2) {
+  return number1 - number2;
+};
+
+var multiply = function(number1, number2) {
+  return number1 * number2;
+};
+
+var divide = function(number1, number2) {
+  return number1 / number2;
+};
+
+// User Interface (front-end) Logic:
+$(document).ready(function() {
+  var number1 = parseInt($("#add1").val());
+  var number2 = parseInt($("#add2").val());
+  alert(add(number1, number2));
+});
