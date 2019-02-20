cc974bca72da8db6689f4ecd2e3ccd80c97b45a4
lizKimita <kimita.wanjiku@gmail.com>
2019-02-20 12:40:14 +0300

    adding calculator changes

--- js/scripts.js
+++ js/scripts.js
@@ -17,7 +17,11 @@ var divide = function(number1, number2) {
 
 // User Interface (front-end) Logic:
 $(document).ready(function() {
+  $("form#add").submit(function(event){
+    event.preventDefault();
   var number1 = parseInt($("#add1").val());
   var number2 = parseInt($("#add2").val());
-  alert(add(number1, number2));
+  var result = add(number1,number2);
+  $("#output").text(result);
+});
 });
